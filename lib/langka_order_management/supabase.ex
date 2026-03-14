87f060e690aff127160386daaf538b311201a71c
UtaW1 <agentkeller2935@gmail.com>
2026-03-14 20:16:54 +0700

    chore: update update supabase api

--- lib/langka_order_management/supabase.ex
+++ lib/langka_order_management/supabase.ex
@@ -74,6 +74,8 @@ defmodule LangkaOrderManagement.Supabase do
   defp try_upload_as_new_file(pass, _), do: pass
 
   defp replace_file_if_exist({:error, :duplicate, {client, bucket_name, file_path, file_data}}) do
+    encoded_path = URI.encode(file_path)
+
     case Tesla.put(client, "/object/#{bucket_name}/#{file_path}", file_data) do
       {:ok, %Tesla.Env{status: 200}} ->
         {:ok, "supabase-s3:#{bucket_name}:#{file_path}"}
